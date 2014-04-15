30dbe5f1aee14e8dd3811d5fe204d2cb64e887fa
Eric Rochester <erochest@virginia.edu>
2014-04-15 16:58:47 -0400

    Started conversion to TypeScript.

--- j/soundscapes.js
+++ j/soundscapes.js
@@ -1,3 +1,6 @@
+/// <reference path="../typings/d3/d3.d.ts" />
+/// <reference path="../typings/wavesurfer/wavesurfer.d.ts" />
+"use strict";
 // Create a wavesurfer object.
 var wavesurfer = Object.create(WaveSurfer);
 
@@ -14,16 +17,10 @@ wavesurfer.init({
 var width = 900, height = 600;
 
 // Use the force...layout.
-var force = d3.layout.force()
-    .linkDistance(55)
-    .charge(-150)
-    .size([width, height]);
+var force = d3.layout.force().linkDistance(55).charge(-150).size([width, height]);
 
 // Find the #clips div and append a SVG tag to it.
-var svg = d3.select("#clips").append("svg")
-    .attr("width", width)
-    .attr("height", height);
-
+var svg = d3.select("#clips").append("svg").attr("width", width).attr("height", height);
 
 // Set up a simple play/pause button for the player.
 var button = d3.select('#play').on('click', function (e) {
@@ -31,155 +28,139 @@ var button = d3.select('#play').on('click', function (e) {
 });
 
 // Read our clips.json data to generate our graph and play audio.
-d3.json("clips.json", function(error, graph) {
+d3.json("clips.json", function (error, graph) {
     //console.log(error);
     //console.log(graph);
-
-    force
-      .nodes(graph.nodes)
-      .links(graph.links)
-      .start();
+    window['graph'] = graph;
+    force.nodes(graph.nodes).links(graph.links).start();
 
     // Current node and category, for player.
-    var currentNode = 0;
-    var currentCategory = null;
-    var index;
+    var current = {
+        node: 0,
+        category: null,
+        index: null
+    };
 
-    var options = d3.selectAll('#options li')
-                    .on('click', function() {
-                        options.classed('current', false);
-                        d3.select(this).classed('current', true);
+    var options = d3.selectAll('#options li').on('click', function () {
+        options.classed('current', false);
+        d3.select(this).classed('current', true);
 
-                        if (index != null) {
-                            id = graph.nodes[index].name;
-                            d3.select('#'+id).classed('current', false);
-                        }
-                        var categoryOption = d3.select(this).attr('data-category');
+        if (current.index != null) {
+            var id = graph.nodes[current.index].name;
+            d3.select('#' + id).classed('current', false);
+        }
+        var categoryOption = d3.select(this).attr('data-category');
 
-                        if (categoryOption == 'all') {
+        if (categoryOption == 'all') {
+            current.category = null;
+        } else {
+            current.category = categoryOption;
+        }
 
-                            currentCategory = null;
+        // Get the file clip for the current node and category.
+        moveToPlayableIndex(current, graph);
 
-                        } else {
-
-                            currentCategory = categoryOption;
-
-                        }
-
-                        // Get the file clip for the current node and category.
-                        index = getIndexToPlay(currentNode, graph, currentCategory);
-
-                        // Tell wavesurfer to load the file.
-                        wavesurfer.load('clips/' + graph.nodes[index].file);
-                    });
+        // Tell wavesurfer to load the file.
+        wavesurfer.load('clips/' + graph.nodes[current.index].file);
+    });
 
     // When wavesurfer is finished playing the file, we'll loop to the next one.
-    wavesurfer.on('ready', function() {
-        var id = graph.nodes[index].name;
+    wavesurfer.on('ready', function () {
+        var id = graph.nodes[current.index].name;
 
-        d3.select('#' + id)
-            .classed('current', true);
+        d3.select('#' + id).classed('current', true);
 
         wavesurfer.play();
     });
 
-    wavesurfer.on('finish', function() {
-        var id = graph.nodes[index].name;
-        d3.select('#'+id).classed('current', false);
+    wavesurfer.on('finish', function () {
+        var id = graph.nodes[current.index].name;
+        d3.select('#' + id).classed('current', false);
 
         while (true) {
-            currentNode = getNextNodeIndex(currentNode, graph);
+            moveToNextNode(current, graph);
 
-            index = getIndexToPlay(currentNode, graph, currentCategory);
+            moveToPlayableIndex(current, graph);
 
-            if (index != null) {
+            if (current.index != null) {
                 break;
             }
 
-            if (currentNode == null) {
+            if (current.node == null) {
                 return;
             }
-
         }
 
-        wavesurfer.load('clips/' + graph.nodes[index].file);
-
+        wavesurfer.load('clips/' + graph.nodes[current.index].file);
     });
 
-    var link = svg.selectAll(".link")
-        .data(graph.links)
-        .enter().append("line")
-        .attr("class", function(link) {
-            if (link[ "path"] > 0) {
-                return "link path" ;
-            } else {
-                return "link" ;
-            }
+    var link = svg.selectAll(".link").data(graph.links).enter().append("line").attr("class", function (link) {
+        if (link["path"] > 0) {
+            return "link path";
+        } else {
+            return "link";
+        }
+    });
+
+    var node = svg.selectAll(".node").data(graph.nodes).enter().append("circle").attr("class", "node").attr("id", function (d) {
+        return d.name;
+    }).attr("r", function (d) {
+        return 5 * d.weight;
+    }).call(force.drag);
+
+    node.append("title").text(function (d) {
+        return d.name;
+    });
+
+    force.on("tick", function () {
+        link.attr("x1", function (d) {
+            return d.source.x;
+        }).attr("y1", function (d) {
+            return d.source.y;
+        }).attr("x2", function (d) {
+            return d.target.x;
+        }).attr("y2", function (d) {
+            return d.target.y;
         });
 
-    var node = svg.selectAll(".node")
-        .data(graph.nodes)
-        .enter().append("circle")
-        .attr("class", "node")
-        .attr("id", function(d) {
-            return d.name
-        })
-        .attr("r", function(d) {
-            return 5 * d.weight;
-        })
-        .call(force.drag)
-
-      node.append("title")
-          .text(function(d) { return d.name; });
-
-    force.on("tick", function() {
-        link.attr("x1", function(d) { return d.source.x; })
-            .attr("y1", function(d) { return d.source.y; })
-            .attr("x2", function(d) { return d.target.x; })
-            .attr("y2", function(d) { return d.target.y; });
-
-        node.attr("cx", function(d) { return d.x; })
-            .attr("cy", function(d) { return d.y; });
+        node.attr("cx", function (d) {
+            return d.x;
+        }).attr("cy", function (d) {
+            return d.y;
+        });
     });
 });
 
 // Returns the index of the next node.
-function getNextNodeIndex(currentIndex, data) {
+function moveToNextNode(pos, data) {
     var nextIndex;
     var links = data.links;
 
-    links.forEach(function(link) {
-        if (link.source.index == currentIndex && link.path == 1) {
-            nextIndex = link.target.index;
+    links.forEach(function (link) {
+        if (link.source.index == pos.index && link.path == 1) {
+            pos.index = link.target.index;
         }
     });
-
-    return nextIndex;
-
 }
 
 // Function to get the file for a node.
-function getIndexToPlay(index, data, category ) {
+function moveToPlayableIndex(pos, data) {
     var nodes, subnodeIndex;
 
     nodes = data.nodes;
 
-    if (category == null) {
-        subnodeIndex = index;
-    } else {
+    if (pos.category != null) {
         var links = data.links;
 
-        links.forEach(function(link) {
-            if (link.target.index == index) {
+        links.forEach(function (link) {
+            if (link.target.index == pos.index) {
                 var subnode = link.source;
 
-                if (subnode.category == category) {
-                    subnodeIndex = subnode.index;
+                if (subnode.category == pos.category) {
+                    pos.index = subnode.index;
                 }
             }
         });
     }
-
-    return subnodeIndex;
 }
-
+//# sourceMappingURL=soundscapes.js.map
---
+++ ts/soundscapes.ts
@@ -0,0 +1,211 @@
+
+/// <reference path="../typings/d3/d3.d.ts" />
+/// <reference path="../typings/wavesurfer/wavesurfer.d.ts" />
+
+"use strict";
+
+interface ClipNode extends D3.Layout.GraphNode {
+    weight: number;
+    name: string;
+    file: string;
+    category: number;
+}
+
+interface ClipLink {
+    source: ClipNode;
+    target: ClipNode;
+    path: number;
+}
+
+interface ClipGraph {
+    nodes: Array<ClipNode>;
+    links: Array<ClipLink>;
+}
+
+interface NodePosition {
+    node: number;
+    category: number;
+    index: number;
+}
+
+// Create a wavesurfer object.
+var wavesurfer = Object.create(WaveSurfer);
+
+// Set wavesurfer options.
+wavesurfer.init({
+    container: document.querySelector('#wave'),
+    waveColor: 'rgba(0,0,0,0.25)',
+    progressColor: 'rgb(0,0,0,0)',
+    cursorColor: 'white',
+    cursorWidth: '4'
+});
+
+// Width and height variables for othe d3 graph.
+var width = 900, height = 600;
+
+// Use the force...layout.
+var force = d3.layout.force()
+    .linkDistance(55)
+    .charge(-150)
+    .size([width, height]);
+
+// Find the #clips div and append a SVG tag to it.
+var svg = d3.select("#clips").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+
+// Set up a simple play/pause button for the player.
+var button = d3.select('#play').on('click', function (e) {
+    wavesurfer.playPause();
+});
+
+// Read our clips.json data to generate our graph and play audio.
+d3.json("clips.json", function(error, graph : ClipGraph) {
+    //console.log(error);
+    //console.log(graph);
+
+    window['graph'] = graph;
+    force
+        .nodes(graph.nodes)
+        .links(graph.links)
+        .start();
+
+    // Current node and category, for player.
+    var current = {
+        node: 0,
+        category: null,
+        index: null
+    };
+
+    var options = d3.selectAll('#options li')
+        .on('click', function() {
+            options.classed('current', false);
+            d3.select(this).classed('current', true);
+
+            if (current.index != null) {
+                var id = graph.nodes[current.index].name;
+                d3.select('#'+id).classed('current', false);
+            }
+            var categoryOption = d3.select(this).attr('data-category');
+
+            if (categoryOption == 'all') {
+
+                current.category = null;
+
+            } else {
+
+                current.category = categoryOption;
+
+            }
+
+            // Get the file clip for the current node and category.
+            moveToPlayableIndex(current, graph);
+
+            // Tell wavesurfer to load the file.
+            wavesurfer.load('clips/' + graph.nodes[current.index].file);
+        });
+
+    // When wavesurfer is finished playing the file, we'll loop to the next one.
+    wavesurfer.on('ready', function() {
+        var id = graph.nodes[current.index].name;
+
+        d3.select('#' + id)
+            .classed('current', true);
+
+        wavesurfer.play();
+    });
+
+    wavesurfer.on('finish', function() {
+        var id = graph.nodes[current.index].name;
+        d3.select('#'+id).classed('current', false);
+
+        while (true) {
+            moveToNextNode(current, graph);
+
+            moveToPlayableIndex(current, graph);
+
+            if (current.index != null) {
+                break;
+            }
+
+            if (current.node == null) {
+                return;
+            }
+
+        }
+
+        wavesurfer.load('clips/' + graph.nodes[current.index].file);
+
+    });
+
+    var link = svg.selectAll(".link")
+        .data(graph.links)
+        .enter().append("line")
+        .attr("class", function(link) {
+            if (link[ "path"] > 0) {
+                return "link path" ;
+            } else {
+                return "link" ;
+            }
+        });
+
+    var node = svg.selectAll(".node")
+        .data(graph.nodes)
+        .enter().append("circle")
+        .attr("class", "node")
+        .attr("id", function(d) {
+            return d.name
+        })
+        .attr("r", function(d) {
+            return 5 * d.weight;
+        })
+        .call(force.drag)
+
+    node.append("title")
+        .text(function(d) { return d.name; });
+
+    force.on("tick", function() {
+        link.attr("x1", function(d) { return d.source.x; })
+            .attr("y1", function(d) { return d.source.y; })
+            .attr("x2", function(d) { return d.target.x; })
+            .attr("y2", function(d) { return d.target.y; });
+
+        node.attr("cx", function(d) { return d.x; })
+            .attr("cy", function(d) { return d.y; });
+    });
+});
+
+// Returns the index of the next node.
+function moveToNextNode(pos : NodePosition, data : ClipGraph) {
+    var nextIndex;
+    var links = data.links;
+
+    links.forEach(function(link: ClipLink) {
+        if (link.source.index == pos.index && link.path == 1) {
+            pos.index = link.target.index;
+        }
+    });
+}
+
+// Function to get the file for a node.
+function moveToPlayableIndex(pos : NodePosition, data : ClipGraph) {
+    var nodes, subnodeIndex;
+
+    nodes = data.nodes;
+
+    if (pos.category != null) {
+        var links = data.links;
+
+        links.forEach(function(link : ClipLink) {
+            if (link.target.index == pos.index) {
+                var subnode = link.source;
+
+                if (subnode.category == pos.category) {
+                    pos.index = subnode.index;
+                }
+            }
+        });
+    }
+}
+
